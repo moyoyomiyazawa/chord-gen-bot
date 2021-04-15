06ba418a76c98c143f78e454ac84589d4ba082df
Yuya Kasai <moyoyomiyazawa@gmail.com>
2021-04-15 15:20:12 +0900

    コードパターンの定数化

---
+++ constants.py
@@ -0,0 +1,1 @@
+CHORD_PATTERN = r'[ ]*[ABCDEFG](?:[\dABCDEFGMm#♭b\-\+△\/\(\), ]|add|omit|sus|dim|aug|on)*'
--- lib/parse_chord_text.py
+++ lib/parse_chord_text.py
@@ -1,14 +1,14 @@
 import re
+import constants
 
 # コード進行テキストをパースして配列にして返す
 def parse_chord_text(chord_text: str) -> list:
     # 小節の区切りで分割する
-    pattern = re.compile(r'[ABCDEFG](?:[\dABCDEFGMm#♭b\-\+△\/\(\), ]|add|omit|sus|dim|aug|on)*')
+    pattern = re.compile(constants.CHORD_PATTERN)
     split_by_barline = [bar.strip() for bar in re.findall(pattern, chord_text)]
-    print(split_by_barline)
 
     # 同一小節内の区切りで分割する
     chord_progression_list = [bar if not re.search(r'\s+', bar) else re.split(r'\s+', bar) for bar in split_by_barline]
-    print(chord_progression_list)
+    print('chord_progression_list:', chord_progression_list)
 
     return chord_progression_list
